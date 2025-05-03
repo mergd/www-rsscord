172379bbafac937e62829eeb3bbb198a0e95e8c4
mergd <24722014+mergd@users.noreply.github.com>
2025-05-02 21:00:55 -0700

    feat: fix og metadata

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const POSTS_DIRECTORY = "blog";
 export const CHANGELOG_DIRECTORY = "changelog";
-export const SITE_NAME = "Template Blog";
+export const SITE_NAME = "discorss";
+export const SITE_DESCRIPTION = "An RSS Bot built for Discord";
--- lib/og/index.ts
+++ lib/og/index.ts
@@ -1,6 +1,6 @@
 import type { Metadata } from "next/types";
 
-import { SITE_NAME } from "../constants";
+import { SITE_DESCRIPTION, SITE_NAME } from "../constants";
 
 export const OpenGraph: Metadata = {
   metadataBase: process.env.NEXT_PUBLIC_SITE_URL
@@ -18,14 +18,14 @@ export const OpenGraph: Metadata = {
     url: process.env.NEXT_PUBLIC_SITE_URL,
     title: SITE_NAME,
     description: "...",
-    images: [`${process.env.NEXT_PUBLIC_SITE_URL}api/og`],
+    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/public/og.png`],
     siteName: SITE_NAME,
   },
   twitter: {
     card: "summary_large_image",
     title: SITE_NAME,
-    description: "...",
-    images: [`${process.env.NEXT_PUBLIC_SITE_URL}api/og`],
+    description: SITE_DESCRIPTION,
+    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/public/og.png`],
   },
   robots: {
     index: true,
